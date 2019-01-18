e4d4f0bf8147658a33da0ec68bee1a879ae61cc1
Shane Chambry <Shaneechambry@gmail.com>
2019-01-17 23:34:18 -0700

    print headers

--- express/server.js
+++ express/server.js
@@ -6,15 +6,20 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+  console.log(req.headers);
+
+  res.status(200).send({ test: "success!" });
 });
+
+
+
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+// app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', router);  // path must route to lambda
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
