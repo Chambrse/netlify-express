659f6e79ec237b19f719f6d0de483952920e2c81
Shane Chambry <Shaneechambry@gmail.com>
2019-01-19 13:01:20 -0700

    meh

--- express/server.js
+++ express/server.js
@@ -24,7 +24,7 @@ router.post('/', (req, res) => {
 router.post('/loadCSV', (req, res) => {
   console.log(req.headers);
   console.log(req.body);
-  console.log(JSON.stringify(req, null, 2));
+  console.log(JSON.stringify(req.body, null, 2));
   console.log("thise is a global variable, testNumber", testNumber);
 
   res.status(200).send({ "text": "csv data received!" });
