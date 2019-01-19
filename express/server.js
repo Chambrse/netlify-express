c15e2760f379f79fbac6a4af716b7db85df74f23
Shane Chambry <Shaneechambry@gmail.com>
2019-01-19 14:17:51 -0700

    reorder bodyparser

--- express/server.js
+++ express/server.js
@@ -6,6 +6,16 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 let testNumber = 42;
+
+
+
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+// app.use('/', router);  // path must route to lambda
+
 router.get('/', (req, res) => {
   console.log(req.headers);
   console.log(req.body);
@@ -23,22 +33,13 @@ router.post('/', (req, res) => {
 
 router.post('/loadCSV', (req, res) => {
   console.log(req.headers);
-  console.log(JSON.stringify(req.route, null, 2));
+  console.log(JSON.stringify(req, null, 2));
   console.log(req.body);
   console.log(req.text);
   console.log("thise is a global variable, testNumber", testNumber);
 
   res.status(200).send({ "text": "csv data received!" });
 });
-
-
-
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-// app.use('/', router);  // path must route to lambda
 
 
 module.exports = app;
