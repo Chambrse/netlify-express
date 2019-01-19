44abc9751eb95ba36c771d4937910d0a84e50c7d
Shane Chambry <Shaneechambry@gmail.com>
2019-01-19 14:27:17 -0700

    added urlencoded bodyparser

--- express/server.js
+++ express/server.js
@@ -13,6 +13,9 @@ router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 // app.use('/', router);  // path must route to lambda
 
